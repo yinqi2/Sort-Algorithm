fe5e5c465fd4b626866152f54b9d044bd443c92a
yinqi <1204623070@qq.com>
2018-06-08 09:27:33 +0800

    插入排序

---
+++ Sort-Algorithm/src/com/yinqi/sort/InsertSort.java
@@ -0,0 +1,28 @@
+package com.yinqi.sort;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		// 测试数组
+		int[] arr = { 4, 6, 3, 2, 5, 9, 0 };
+		insertSort(arr, 7);
+		for (int i = 0; i < 7; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	//插入排序
+	public static void insertSort(int[] arr, int n) {
+
+		for (int i = 1; i < n; i++) {
+			int temp = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
