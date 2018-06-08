feed68a464371cc211a8b63306e72d6f417bda58
yinqi <1204623070@qq.com>
2018-06-08 09:49:11 +0800

    在添加一条注释

--- Sort-Algorithm/src/com/yinqi/sort/InsertSort.java
+++ Sort-Algorithm/src/com/yinqi/sort/InsertSort.java
@@ -17,6 +17,7 @@ public class InsertSort {
 
 		for (int i = 1; i < n; i++) {
 			int temp = arr[i];
+			//从[0~i-1]中寻找正确插入的位置
 			int j = i - 1;
 			while (j >= 0 && arr[j] > temp) {
 				arr[j + 1] = arr[j];
